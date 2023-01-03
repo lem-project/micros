c470974dbbddd04beb6842228d3b38c8f8c2989d
cxxxr <g23tlm@gmail.com>
2023-01-03 23:52:51 +0900

    improve hover-symbol display

--- lsp-api.lisp
+++ lsp-api.lisp
@@ -3,9 +3,70 @@
   (:export :hover-symbol))
 (in-package :lsp-backend/lsp-api)
 
+(defun describe-variable (symbol)
+  (list "Variable"
+        (with-output-to-string (stream)
+          (when (boundp symbol)
+            (sb-impl::describe-variable symbol stream)))))
+
+(defun describe-function (symbol)
+  (list "Function"
+        (with-output-to-string (stream)
+          (when (fboundp symbol)
+            (let ((arglist (cons symbol (lsp-backend/backend:arglist symbol))))
+              (write-line "```lisp" stream)
+              (let ((*print-case* :downcase))
+                (format stream "~(~A~)~%" arglist))
+              (write-line "```" stream)
+              (let ((doc (documentation symbol 'function)))
+                (when doc
+                  (write-line doc stream))))))))
+
+(defun describe-class (symbol)
+  (list "Class"
+        (with-output-to-string (stream)
+          (sb-impl::describe-class symbol nil stream))))
+
+(defun describe-type (symbol)
+  (list "Type"
+        (with-output-to-string (stream)
+          (sb-impl::describe-type symbol stream))))
+
+(defun describe-declaration (symbol)
+  (list "Declaration"
+        (with-output-to-string (stream)
+          (sb-impl::describe-declaration symbol stream))))
+
+(defun describe-plist (symbol)
+  (list "Symbol-plist:"
+        (with-output-to-string (stream)
+          (let ((plist (symbol-plist symbol)))
+            (when plist
+              (loop :for (k v) :on plist :by #'cddr
+                    :do (format stream
+                                "  ~@:_~A -> ~A~%"
+                                (prin1-to-string k)
+                                (prin1-to-string v))))))))
+
 (defun hover-symbol (symbol-name)
   (lsp-backend::with-buffer-syntax ()
     (multiple-value-bind (symbol status)
         (lsp-backend::parse-symbol symbol-name)
-      (when status 
-        (lsp-backend::describe-to-string symbol)))))
+      (when status
+        (with-output-to-string (stream)
+          (let ((contents
+                  (remove ""
+                          (list (describe-variable symbol)
+                                (describe-function symbol)
+                                (describe-class symbol)
+                                (describe-type symbol)
+                                (describe-declaration symbol)
+                                (describe-plist symbol))
+                          :key #'second
+                          :test #'string=)))
+            (loop :for (header body) :in contents
+                  :for first := t :then nil
+                  :do (unless first
+                        (write-line "----------" stream))
+                      (format stream "## ~A~%" header)
+                      (write-line body stream))))))))
