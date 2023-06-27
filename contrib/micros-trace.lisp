0af3ad315c19e4ce68e797733671d9c9eb9aa349
cxxxr <g23tlm@gmail.com>
2023-06-28 00:30:29 +0900

    add trace operators

--- contrib/micros-trace.lisp
+++ contrib/micros-trace.lisp
@@ -2,6 +2,17 @@
   (:use :cl))
 (in-package :micros/trace)
 
+(defvar *traces* '())
+
+(defun already-traced-p (name)
+  (find name *traces*))
+
+(defun remove-trace (name)
+  (setf *traces* (remove name *traces*)))
+
+(defun add-trace (name)
+  (push name *traces*))
+
 (defvar *depth* 0)
 
 (defun print-indentation ()
@@ -11,24 +22,45 @@
 (defun send-write-objects-event (objects)
   (loop :for first := t :then nil
         :for object :in objects
-        :do (unless first
-              (write-char #\space))
+        :do (write-char #\space)
             (micros::send-write-object-event object)))
 
+(defun coerce-to-symbol (name)
+  (etypecase name
+    (symbol name)
+    (string (micros:from-string name))))
+
 (micros/swank-api:defslimefun micros-trace (name)
-  (flet ((before-hook (args)
-           (print-indentation)
-           (format t "(~A " name)
-           (send-write-objects-event args)
-           (format t ")~%")
-           (incf *depth*))
-         (after-hook (retlist)
-           (decf *depth*)
-           (print-indentation)
-           (format t "~A returned " name)
-           (send-write-objects-event retlist)
-           (terpri)))
-    ;; TODO: check already traced
-    (micros/backend:wrap name 'micros-trace
-                         :before #'before-hook
-                         :after #'after-hook)))
+  (let ((name (coerce-to-symbol name)))
+    (when (already-traced-p name)
+      (warn "~a is apparently already traced! Untracing and retracing." name)
+      (micros-untrace name))
+    (flet ((before-hook (args)
+             (print-indentation)
+             (format t "(~A" name)
+             (send-write-objects-event args)
+             (format t ")~%")
+             (incf *depth*))
+           (after-hook (retlist)
+             (decf *depth*)
+             (print-indentation)
+             (format t "~A returned " name)
+             (send-write-objects-event retlist)
+             (terpri)))
+      (micros/backend:wrap name 'micros-trace
+                           :before #'before-hook
+                           :after #'after-hook)
+      (add-trace name)
+      (values))))
+
+(micros/swank-api:defslimefun micros-untrace (name)
+  (let ((name (coerce-to-symbol name)))
+    (micros/backend:unwrap name 'micros-trace)
+    (remove-trace name)
+    (values)))
+
+(micros/swank-api:defslimefun micros-trace-list ()
+  (mapcar (lambda (name)
+            (let ((*package* micros::*swank-io-package*))
+              (prin1-to-string name)))
+          *traces*))
